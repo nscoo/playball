06b7bf57fc9f156e96c120344e9c073af16b0175
nscoo <nscoo@naver.com>
2022-09-03 14:32:56 +0900

    Initial commit

---
+++ src/main/java/com/project/playball/PlayballApplication.java
@@ -0,0 +1,13 @@
+package com.project.playball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/playball/PlayballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.playball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
